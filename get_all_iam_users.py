f36dcb91e57605e29eab6018eba051f861bfec4a
Prajjwal <prajjwalpawar1@gmail.com>
2023-12-26 16:43:19 +0530

    boto3 practice

---
+++ get_all_iam_users.py
@@ -0,0 +1,17 @@
+# Import all modules OR libs
+import boto3
+
+# Accessing the management console programatically
+aws_management_console = boto3.session.Session(profile_name="default")
+
+# Open IAM Console
+iam_console_resource  = aws_management_console.resource(service_name="iam")
+iam_console_client = aws_management_console.client(service_name="iam")
+
+# IAM list with resource object
+for each_user in iam_console_resource.users.all():
+    print(each_user.name)
+
+# IAM list with client object
+for each_user in iam_console_client.list_users()["Users"]:
+    print(each_user["UserName"])
---
+++ get_all_running_ec2.py
@@ -0,0 +1,12 @@
+# Get all Modules & libs
+import boto3
+
+# Open AWS management console
+aws_management_console = boto3.session.Session(profile_name="default")
+
+# Open EC2 Console
+ec2_console = aws_management_console.client(service_name="ec2", region_name="us-east-1")
+
+# Use boto3 Docs for more info (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html)
+result = ec2_console.describe_instances()["Reservations"]
+print(result)
